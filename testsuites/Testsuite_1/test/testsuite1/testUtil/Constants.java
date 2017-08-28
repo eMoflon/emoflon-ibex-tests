02093dd28feec04005c8ff25fd7cb27251602437
Florian Stolte <fstolte@mail.upb.de>
2017-08-28 16:36:42 +0200

    Prepared testsuite project for performance tests.

--- testsuites/Testsuite_1/test/testsuite1/testUtil/Constants.java
+++ testsuites/Testsuite_1/test/testsuite1/testUtil/Constants.java
@@ -2,4 +2,13 @@ package testsuite1.testUtil;
 
 public class Constants {
 	public final static String workpacePath = "./../../tggs/";
+	
+	public final static String[] testProjects = {"BlockCodeAdapter",
+											     "BlockDiagramCodeAdapter",
+											     "ClassInhHier2DB",
+											     "CompanyToIT",
+											     "FamiliesToPersons_V0",
+											     "FamiliesToPersons_V1",
+											     "ProcessCodeAdapter",
+											     "VHDLTGGCodeAdapter"};
 }
