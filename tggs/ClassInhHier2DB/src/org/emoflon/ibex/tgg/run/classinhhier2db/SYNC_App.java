5445fb82bbae32f83a02f7ad8fd60b091ff95206
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-09-10 23:28:43 +0200

    Added main method

--- tggs/ClassInhHier2DB/src/org/emoflon/ibex/tgg/run/classinhhier2db/SYNC_App.java
+++ tggs/ClassInhHier2DB/src/org/emoflon/ibex/tgg/run/classinhhier2db/SYNC_App.java
@@ -1,6 +1,8 @@
 package org.emoflon.ibex.tgg.run.classinhhier2db;
 
 import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.operational.strategies.sync.SYNC;
 import org.emoflon.ibex.tgg.runtime.engine.DemoclesEngine;
@@ -13,6 +15,21 @@ public class SYNC_App extends SYNC {
 	public SYNC_App(String projectName, String workspacePath, boolean flatten, boolean debug) throws IOException {
 		super(projectName, workspacePath, flatten, debug);
 		registerPatternMatchingEngine(new DemoclesEngine());
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+
+		SYNC_App sync = new SYNC_App("ClassInhHier2DB", "./../", false, true);
+		
+		logger.info("Starting SYNC");
+		long tic = System.currentTimeMillis();
+		sync.forward();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed SYNC in: " + (toc - tic) + " ms");
+		
+		sync.saveModels();
+		sync.terminate();
 	}
 	
 	@Override
