b6477f7ede086dc7d4bf90dec5a8b22e3bd910b4
nilsweid <nilsweid@mail.upb.de>
2017-12-06 14:39:12 +0100

    Merge new master into the branch

---
+++ tggs/ClassInhHier2DB_MA/src/org/emoflon/ibex/tgg/run/classinhhier2db_ma/CO_App.java
@@ -0,0 +1,40 @@
+package org.emoflon.ibex.tgg.run.classinhhier2db_ma;
+
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.commons.lang3.NotImplementedException;
+
+import org.emoflon.ibex.tgg.operational.strategies.co.CO;
+import org.emoflon.ibex.tgg.runtime.engine.DemoclesEngine;
+
+public class CO_App extends CO {
+
+	public CO_App(String projectName, String workspacePath, boolean debug) throws IOException {
+		super(projectName, workspacePath, debug);
+		registerPatternMatchingEngine(new DemoclesEngine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+
+		CO_App co = new CO_App("ClassInhHier2DB_MA", "./../", false);
+		
+		logger.info("Starting CO");
+		long tic = System.currentTimeMillis();
+		co.run();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed CO in: " + (toc - tic) + " ms");
+		
+		co.saveModels();
+		co.terminate();
+	}
+
+	protected void registerUserMetamodels() throws IOException {
+		// Load and register source and target metamodels
+		throw new NotImplementedException("Please check that your source and target metamodels are loaded and registered.");
+		
+		// Register correspondence metamodel last
+		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
+	}
+}
