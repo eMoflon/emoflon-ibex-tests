1cb1084122ce973b9893edc702d2860ffc73704a
Lars Fritsche <arikae00@jesitec.de>
2017-12-10 19:37:49 +0100

    Recommit of last merge (not everything was commited)

--- tggs/BenchmarxFamiliesToPersons/src/org/emoflon/ibex/tgg/run/benchmarxfamiliestopersons/CO_App.java
+++ tggs/BenchmarxFamiliesToPersons/src/org/emoflon/ibex/tgg/run/benchmarxfamiliestopersons/CO_App.java
@@ -3,8 +3,9 @@ package org.emoflon.ibex.tgg.run.benchmarxfamiliestopersons;
 import java.io.IOException;
 
 import org.apache.log4j.BasicConfigurator;
-
+import org.emoflon.ibex.tgg.operational.csp.constraints.factories.UserDefinedRuntimeTGGAttrConstraintFactory;
 import org.emoflon.ibex.tgg.operational.strategies.co.CO;
+import org.emoflon.ibex.tgg.operational.util.IbexOptions;
 import org.emoflon.ibex.tgg.runtime.engine.DemoclesEngine;
 
 import Families.impl.FamiliesPackageImpl;
@@ -13,7 +14,7 @@ import Persons.impl.PersonsPackageImpl;
 public class CO_App extends CO {
 
 	public CO_App(String projectName, String workspacePath, boolean debug) throws IOException {
-		super(projectName, workspacePath, debug);
+		super(createIbexOptions().projectName(projectName).workspacePath(workspacePath).debug(debug));
 		registerPatternMatchingEngine(new DemoclesEngine());
 	}
 
@@ -39,5 +40,13 @@ public class CO_App extends CO {
 		
 		// Register correspondence metamodel last
 		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
+	}
+	
+	protected static IbexOptions createIbexOptions() {
+		IbexOptions options = new IbexOptions();
+		options.projectName("BenchmarxFamiliesToPersons");
+		options.debug(true);
+		options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+		return options;
 	}
 }
--- tggs/CompanyToIT/src/org/emoflon/ibex/tgg/run/companytoit/CO_App.java
+++ tggs/CompanyToIT/src/org/emoflon/ibex/tgg/run/companytoit/CO_App.java
@@ -3,8 +3,9 @@ package org.emoflon.ibex.tgg.run.companytoit;
 import java.io.IOException;
 
 import org.apache.log4j.BasicConfigurator;
-
+import org.emoflon.ibex.tgg.operational.csp.constraints.factories.UserDefinedRuntimeTGGAttrConstraintFactory;
 import org.emoflon.ibex.tgg.operational.strategies.co.CO;
+import org.emoflon.ibex.tgg.operational.util.IbexOptions;
 import org.emoflon.ibex.tgg.runtime.engine.DemoclesEngine;
 
 import CompanyLanguage.impl.CompanyLanguagePackageImpl;
@@ -13,7 +14,7 @@ import ITLanguage.impl.ITLanguagePackageImpl;
 public class CO_App extends CO {
 
 	public CO_App(String projectName, String workspacePath, boolean debug) throws IOException {
-		super(projectName, workspacePath, debug);
+		super(createIbexOptions().projectName(projectName).workspacePath(workspacePath).debug(debug));
 		registerPatternMatchingEngine(new DemoclesEngine());
 	}
 
@@ -39,5 +40,13 @@ public class CO_App extends CO {
 		
 		// Register correspondence metamodel last
 		loadAndRegisterMetamodel(projectPath + "/model/" + projectPath + ".ecore");
+	}
+	
+	private static IbexOptions createIbexOptions() {
+		IbexOptions options = new IbexOptions();
+		options.projectName("CompanyToIT");
+		options.debug(true);
+		options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+		return options;
 	}
 }
