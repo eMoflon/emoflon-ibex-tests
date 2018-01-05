fee89a817a40466c9e3455545a5adc2764302d72
smilicas <milica@mail.uni-paderborn.de>
2018-01-05 17:50:20 +0100

    detailed sync TCs for C2DB with inheritance added

--- metamodels/Database/src/org/benchmarx/database/core/DatabaseHelper.java
+++ metamodels/Database/src/org/benchmarx/database/core/DatabaseHelper.java
@@ -1,5 +1,9 @@
 package org.benchmarx.database.core;
 
+import java.util.Optional;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
 import Database.Column;
 import Database.DB;
 import Database.DatabaseFactory;
@@ -23,5 +27,33 @@ public class DatabaseHelper {
 		Column c = DatabaseFactory.eINSTANCE.createColumn();
 		t.getColumns().add(c);
 		c.setName(name);
+	}
+	
+	public void deleteTable(DB db, String name){
+		Table toBeDeleted = db.getTables().stream()
+				.filter(t -> t.getHeading().equals(name)).findAny().get();
+		EcoreUtil.delete(toBeDeleted);
+	}
+	
+	public void deleteColumnFromTable(DB db, String name, String tableName){
+		Table table = db.getTables().stream()
+				.filter(t -> t.getHeading().equals(tableName)).findAny().get();
+		Optional<Column> toBeDeleted = table.getColumns().stream()
+				.filter(t -> t.getName().equals(name)).findAny();
+		toBeDeleted.ifPresent(t -> EcoreUtil.delete(t));
+	}
+	
+	public void renameTable(DB db, String name, String newName){
+		Optional<Table> toBeRenamed = db.getTables().stream()
+				.filter(t -> t.getHeading().equals(name)).findAny();
+		toBeRenamed.ifPresent(t -> t.setHeading(newName));
+	}
+	
+	public void renameColumnFromTable(DB db, String name, String newName, String tableName){
+		Table table = db.getTables().stream()
+				.filter(t -> t.getHeading().equals(name)).findAny().get();
+		Optional<Column> toBeDeleted = table.getColumns().stream()
+				.filter(t -> t.getName().equals(name)).findAny();
+		toBeDeleted.ifPresent(t -> t.setName(newName));
 	}
 }
---
+++ testsuites/Testsuite/test/testsuite/ibex/ClassInhHier2DB_MA/sync/util/SyncTestCaseCD2DB.java
@@ -0,0 +1,117 @@
+package testsuite.ibex.ClassInhHier2DB_MA.sync.util;
+
+import java.util.Optional;
+
+import org.benchmarx.classInheritanceHierarchy.core.ClassInheritanceHierarchyHelper;
+import org.benchmarx.database.core.DatabaseHelper;
+import org.emoflon.ibex.tgg.operational.util.IMatch;
+import org.emoflon.ibex.tgg.operational.util.ImmutableMatchContainer;
+import org.emoflon.ibex.tgg.operational.util.UpdatePolicy;
+import org.junit.Ignore;
+
+import ClassInheritanceHierarchy.ClassPackage;
+import Database.DB;
+import testsuite.ibex.testUtil.SyncTestCase;
+
+@Ignore("Waiting for sync to be finished")
+public class SyncTestCaseCD2DB extends SyncTestCase<ClassPackage, DB>{
+
+public final static String projectName = "ClassInhHier2DB_MA";
+	
+	public SyncTestCaseCD2DB() {
+		super(new IbexCD2DB_MA(projectName));
+	}
+	
+	protected ClassInheritanceHierarchyHelper helperClass;
+	protected DatabaseHelper helperDB;
+	
+	@Override
+	protected String getProjectName() {
+		return projectName;
+	}
+
+	@Override
+	protected void initHelpers() {
+		helperClass = new ClassInheritanceHierarchyHelper();
+		helperDB = new DatabaseHelper();
+	}
+	
+	public void setPreferClassUpdatePolicy() {
+		((IbexCD2DB_MA) tool).getSync().setUpdatePolicy(new UpdatePolicy() {
+			@Override
+			public IMatch chooseOneMatch(ImmutableMatchContainer matchContainer) {
+				return getMatch(matchContainer, "ClassToTableRule");
+			}
+		});
+	}
+	
+	public void setPreferSubClassUpdatePolicy() {
+		((IbexCD2DB_MA) tool).getSync().setUpdatePolicy(new UpdatePolicy() {
+			@Override
+			public IMatch chooseOneMatch(ImmutableMatchContainer matchContainer) {
+				return getMatch(matchContainer, "SubClassToTable");
+			}
+		});
+	}
+	
+	public IMatch getMatch(ImmutableMatchContainer matchContainer, String preferedRule) {
+		Optional<IMatch> match = matchContainer.getMatches()
+				.stream()
+				.filter(m -> m.patternName().contains(preferedRule))
+				.findAny();
+		return match.orElse(matchContainer.getNextRandom());
+	}
+	
+	// ---- FWD ----
+	
+	public void createInheritance() {
+		tool.performAndPropagateSourceEdit(util.execute( (ClassPackage p) -> helperClass.createClass(p, "C1"))
+				   .andThen(p -> helperClass.createSubClass(p, "SC1", 0))
+			);
+		//------------
+		assertPostcondition("in/04_SubClassToTable_FWD", "expected/04_SubClassToTable_FWD");
+	}
+	
+	public void createInheritanceWithAttributes() {
+		tool.performAndPropagateSourceEdit(util.execute( (ClassPackage p) -> helperClass.createClass(p, "C1"))
+				.andThen( p -> helperClass.createAttributeInClass(p, "a1", null, 0))
+				.andThen( p -> helperClass.createSubClass(p, "SC1", 0))
+				.andThen( p -> helperClass.createAttributeInClass(p, "a2", null, 1)));
+		//------------
+		assertPostcondition("in/06_OneSuperAtt_OneSubAtt_FWD", "expected/06_OneSuperAtt_OneSubAtt_FWD");
+	}
+	
+	
+	// ---- BWD ----
+	public void createTables(){
+		assertPrecondition("in/01_PackageToDatabase_FWD", "expected/01_PackageToDatabase_FWD");
+		//------------
+		setPreferClassUpdatePolicy();
+		tool.performAndPropagateTargetEdit(util.execute( (DB db) -> helperDB.createTable(db, "C1"))
+				.andThen( db -> helperDB.createTable(db, "SC1")));
+		//------------
+		assertPostcondition("in/SubClassToTable_BWD", "expected/SubClassToTable_BWD");
+	}
+	
+	public void createTablesWithColumns() {
+		setPreferClassUpdatePolicy();
+		tool.performAndPropagateTargetEdit(util.execute( (DB db) -> helperDB.createTable(db, "C1"))
+				.andThen(db -> helperDB.createColumnInTable(db, "a1", 0))
+				.andThen( db -> helperDB.createTable(db, "SC1"))
+				.andThen(db -> helperDB.createColumnInTable(db, "a2", 1)));
+		//------------
+		assertPostcondition("in/SubClassToTableWithColumns_BWD", "expected/SubClassToTableWithColumns_BWD");
+	}
+	
+	//this is tricky because you do not if C1->SC1 or SC1->C1
+	public void createTablesWithInheritance(){
+		assertPrecondition("in/01_PackageToDatabase_FWD", "expected/01_PackageToDatabase_FWD");
+		//------------
+		setPreferSubClassUpdatePolicy();
+		tool.performAndPropagateTargetEdit(util.execute( (DB db) -> helperDB.createTable(db, "C1"))
+				.andThen( db -> helperDB.createTable(db, "SC1")));
+		//------------
+		assertPostcondition("in/SubClassToTable_BWD", "expected/SubClassToTable_BWD");
+	}
+
+}
