09151bb65fb74fe63cd4bde50ab04c8e3094d1ea
smilicas <milica@mail.uni-paderborn.de>
2018-01-07 12:41:59 +0100

    sync TCs for Class2Doc created

--- metamodels/Documents/src/org/benchmarx/documents/core/DocumentsHelper.java
+++ metamodels/Documents/src/org/benchmarx/documents/core/DocumentsHelper.java
@@ -1,5 +1,7 @@
 package org.benchmarx.documents.core;
 
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
 import Documents.Container;
 import Documents.Document;
 import Documents.DocumentsFactory;
@@ -12,12 +14,26 @@ public class DocumentsHelper {
 		c.getDocuments().add(doc);
 	}
 	
-	public void createReference(Container c, String doc1, String doc2) {
+	public void createReference(Container c, String from, String to) {
+		Document d1 = c.getDocuments().stream()
+				.filter(a -> a.getName().equals(from)).findAny().get();
+		Document d2 = c.getDocuments().stream()
+				.filter(a -> a.getName().equals(to)).findAny().get();
+		d1.getReference().add(d2);
+	}
+	
+	public void deleteDocument(Container c, String name) {
+		Document toBeDeleted = c.getDocuments().stream()
+				.filter(p -> p.getName().equals(name)).findAny().get();
+		EcoreUtil.delete(toBeDeleted, true);
+	}
+	
+	public void deleteReference(Container c, String doc1, String doc2) {
 		Document d1 = c.getDocuments().stream()
 				.filter(a -> a.getName().equals(doc1)).findAny().get();
 		Document d2 = c.getDocuments().stream()
 				.filter(a -> a.getName().equals(doc2)).findAny().get();
-		d1.getReference().add(d2);
+		d1.getReference().remove(d2);
 	}
 
 }
