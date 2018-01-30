92f64f9d73367e7305619d722d2b8c74238fb293
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-01-30 21:47:01 +0100

    Added new generated register helpers (to be refactored later)

---
+++ tggs/ProcessCodeAdapter/src/org/emoflon/ibex/tgg/run/processcodeadapter/_RegistrationHelper.java
@@ -0,0 +1,20 @@
+package org.emoflon.ibex.tgg.run.processcodeadapter;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+public class _RegistrationHelper {
+
+	/** Load and register source and target metamodels */
+	public static void registerMetamodels(ResourceSet rs) {
+		throw new NotImplementedException("You need to register your source and target metamodels.");
+		
+		// For both source and target metamodels (and any other dependencies you might require)
+		
+		// Option 1 (recommended): If you have generated code for your metamodel <Foo> and use eMoflon projects and defaults
+		// rs.getPackageRegistry().put("platform:/resource/Foo/model/Foo.ecore", FooPackageImpl.init());
+
+		// Option 2:  If you wish to use the .ecore file directly without generating code
+		// loadAndRegisterMetamodel(<pathToEcoreFile>);
+	}
+}
--- tggs/VHDLTGGCodeAdapter/src/org/emoflon/ibex/tgg/operational/csp/constraints/factories/UserDefinedRuntimeTGGAttrConstraintFactory.java
+++ tggs/VHDLTGGCodeAdapter/src/org/emoflon/ibex/tgg/operational/csp/constraints/factories/UserDefinedRuntimeTGGAttrConstraintFactory.java
@@ -1,20 +1,20 @@
-		package org.emoflon.ibex.tgg.operational.csp.constraints.factories;
-		
-		import java.util.HashMap;
-		import java.util.HashSet;			
+package org.emoflon.ibex.tgg.operational.csp.constraints.factories;
 
-		
-		public class UserDefinedRuntimeTGGAttrConstraintFactory extends RuntimeTGGAttrConstraintFactory {
-		
-			public UserDefinedRuntimeTGGAttrConstraintFactory() {
-				super();
-			}
-			
-			@Override
-			protected void initialize() {
-				creators = new HashMap<>();
-				
-				constraints = new HashSet<String>();
-				constraints.addAll(creators.keySet());
-			}
-		}
+import java.util.HashMap;
+import java.util.HashSet;			
+
+
+public class UserDefinedRuntimeTGGAttrConstraintFactory extends RuntimeTGGAttrConstraintFactory {
+
+	public UserDefinedRuntimeTGGAttrConstraintFactory() {
+		super();
+	}
+	
+	@Override
+	protected void initialize() {
+		creators = new HashMap<>();
+
+		constraints = new HashSet<String>();
+		constraints.addAll(creators.keySet());
+	}
+}
