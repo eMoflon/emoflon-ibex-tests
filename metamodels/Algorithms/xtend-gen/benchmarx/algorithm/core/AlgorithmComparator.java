522a5b8a7641a46df8d10c055545d1d4b2f5cd61
nilsweid <nilsweid@mail.upb.de>
2018-03-15 10:55:34 +0100

    Delete MoDisoIbexTGG protocol

--- metamodels/Algorithms/xtend-gen/benchmarx/algorithm/core/AlgorithmComparator.java
+++ metamodels/Algorithms/xtend-gen/benchmarx/algorithm/core/AlgorithmComparator.java
@@ -1,26 +1,58 @@
 package benchmarx.algorithm.core;
 
+import Algorithms.Algorithm;
+import Algorithms.AlgorithmContainer;
+import java.util.ArrayList;
+import java.util.List;
 import org.benchmarx.emf.Comparator;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.junit.Assert;
 
 @SuppressWarnings("all")
-public class AlgorithmComparator /* implements Comparator<AlgorithmContainer>  */{
+public class AlgorithmComparator implements Comparator<AlgorithmContainer> {
   /**
    * checkAttributeValues determines if equality requires equal values in all EAttributes.
    * MODELGEN tests cannot handle this and therefore require checkAttributeValues == false.
    */
   @Override
-  public void assertEquals(final /* AlgorithmContainer */Object expected, final /* AlgorithmContainer */Object actual) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method stringify(AlgorithmContainer) from the type AlgorithmComparator refers to the missing type AlgorithmContainer"
-      + "\nThe method assertEquals(AlgorithmContainer, AlgorithmContainer) from the type AlgorithmComparator refers to the missing type AlgorithmContainer"
-      + "\nThe method stringify(AlgorithmContainer) from the type AlgorithmComparator refers to the missing type AlgorithmContainer"
-      + "\nThe method stringify(AlgorithmContainer) from the type AlgorithmComparator refers to the missing type AlgorithmContainer");
+  public void assertEquals(final AlgorithmContainer expected, final AlgorithmContainer actual) {
+    Assert.assertTrue(this.stringify(expected).startsWith("AlgorithmContainer"));
+    Assert.assertEquals(this.stringify(expected), this.stringify(actual));
   }
   
-  public String stringify(final /* AlgorithmContainer */Object AlgorithmContainer) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nAlgorithm cannot be resolved to a type."
-      + "\nAlgorithm cannot be resolved to a type."
-      + "\nalgorithms cannot be resolved");
+  public String stringify(final AlgorithmContainer AlgorithmContainer) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("AlgorithmContainer {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("name = \"AlgorithmContainer\",");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("algorithms = [");
+    _builder.newLine();
+    _builder.append("\t");
+    EList<Algorithm> _algorithms = AlgorithmContainer.getAlgorithms();
+    final List<Algorithm> sortedList = new ArrayList<Algorithm>(_algorithms);
+    _builder.newLineIfNotEmpty();
+    {
+      boolean _hasElements = false;
+      for(final Algorithm c : sortedList) {
+        if (!_hasElements) {
+          _hasElements = true;
+        } else {
+          _builder.appendImmediate(", ", "\t");
+        }
+        _builder.append("\t");
+        _builder.append("a", "\t");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("\t");
+    _builder.append("]");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    return _builder.toString();
   }
 }
