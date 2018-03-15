a5ddbce51d9dc2828c31927d105620d8f589cee4
Anthony Anjorin <anthony.anjorin@upb.de>
2018-03-15 15:38:44 +0100

    Removed bin

--- tggs/MoDiscoIbexTGG/src/org/emoflon/ibex/tgg/run/modiscoibextgg/_RegistrationHelper.java
+++ tggs/MoDiscoIbexTGG/src/org/emoflon/ibex/tgg/run/modiscoibextgg/_RegistrationHelper.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.emoflon.ibex.tgg.operational.strategies.OPT;
 
-
 public class _RegistrationHelper {
 
 	/** Load and register source and target metamodels */
-	//public static void registerMetamodels(ResourceSet rs, OPT strategy)  throws IOException {
+	public static void registerMetamodels(ResourceSet rs, OPT strategy)  throws IOException {
 		
-	//}
+	}
 }
