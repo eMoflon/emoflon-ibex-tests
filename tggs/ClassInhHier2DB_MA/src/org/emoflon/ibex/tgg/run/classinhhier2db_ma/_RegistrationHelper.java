37eeb5d9f3e38baaa898e2f6b2163b98b4ead2c0
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-05-15 23:11:09 +0200

    Fixes to get workspace compiling

---
+++ tggs/ClassInhHier2DB_MA/src/org/emoflon/ibex/tgg/run/classinhhier2db_ma/_RegistrationHelper.java
@@ -0,0 +1,46 @@
+package org.emoflon.ibex.tgg.run.classinhhier2db_ma;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.emoflon.ibex.tgg.operational.strategies.OperationalStrategy;
+
+import org.emoflon.ibex.tgg.operational.csp.constraints.factories.UserDefinedRuntimeTGGAttrConstraintFactory;
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.operational.strategies.OperationalStrategy;
+
+public class _RegistrationHelper {
+
+	/** Load and register source and target metamodels */
+	public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy)  throws IOException {
+		throw new NotImplementedException("You need to register your source and target metamodels.");
+		
+		// For both source and target metamodels (and any other dependencies you might require)
+		
+		// Option 1 (recommended): If you have generated code for your metamodel <Foo> and use eMoflon projects and defaults,
+		//                         just add the project Foo as a plugin dependency and simply use:
+		// FooPackageImpl.init();
+
+		// Option 2:  If you wish to use the .ecore file directly without generating code
+		// strategy.loadAndRegisterMetamodel("<pathToEcoreFile>");
+		
+		// Option 3 (advanced): If you have an .ecore file with an arbitrary URI "<URIOfPackage>"
+		// String pathToEcoreFile = "<pathToEcoreFile>";
+		// URI key = URI.createURI("<URIOfPackage>");
+		// URI value = URI.createURI(pathToEcoreFile);
+		// strategy.loadAndRegisterMetamodel(pathToEcoreFile);
+		// rs.getURIConverter().getURIMap().put(key, value);
+	}
+	
+	/** Create default options **/
+	public static IbexOptions createIbexOptions() {
+		IbexOptions options = new IbexOptions();
+		options.projectName("ClassInhHier2DB_MA");
+		options.projectPath("ClassInhHier2DB_MA");
+		options.debug(false);
+		options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+		return options;
+	}
+}
--- tggs/MocaTreeToProcess/src/org/emoflon/ibex/tgg/run/mocatreetoprocess/_RegistrationHelper.java
+++ tggs/MocaTreeToProcess/src/org/emoflon/ibex/tgg/run/mocatreetoprocess/_RegistrationHelper.java
@@ -1,6 +1,9 @@
 package org.emoflon.ibex.tgg.run.mocatreetoprocess;
 
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.emoflon.ibex.tgg.operational.csp.constraints.factories.UserDefinedRuntimeTGGAttrConstraintFactory;
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.operational.strategies.OperationalStrategy;
 
 import MocaTree.impl.MocaTreePackageImpl;
 import ProcessDefinition.impl.ProcessDefinitionPackageImpl;
@@ -8,9 +11,19 @@ import ProcessDefinition.impl.ProcessDefinitionPackageImpl;
 public class _RegistrationHelper {
 
 	/** Load and register source and target metamodels */
-	public static void registerMetamodels(ResourceSet rs) {
+	public static void registerMetamodels(ResourceSet rs, OperationalStrategy op) {
 		// Load and register source and target metamodels
 		rs.getPackageRegistry().put("platform:/resource/MocaTree/model/MocaTree.ecore", MocaTreePackageImpl.init());
 		rs.getPackageRegistry().put("platform:/resource/ProcessDefinition/model/ProcessDefinition.ecore", ProcessDefinitionPackageImpl.init());
+	}
+	
+	/** Create default options **/
+	public static IbexOptions createIbexOptions() {
+		IbexOptions options = new IbexOptions();
+		options.projectName("MocaTreeToProcess");
+		options.projectPath("MocaTreeToProcess");
+		options.debug(false);
+		options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+		return options;
 	}
 }
