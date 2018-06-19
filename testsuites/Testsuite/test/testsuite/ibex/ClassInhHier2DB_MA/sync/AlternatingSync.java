4469bfea995ca571e6d15cfef707d9ae41305d11
Patrick Robrecht <mail@patrick-robrecht.de>
2018-06-19 18:21:57 +0200

    Ignore testsuite.ibex.ClassInhHier2DB_MA.sync.AlternatingSync.testCreateSubClassDeleteSubTable (failed in 21st execution)

--- testsuites/Testsuite/test/testsuite/ibex/ClassInhHier2DB_MA/sync/AlternatingSync.java
+++ testsuites/Testsuite/test/testsuite/ibex/ClassInhHier2DB_MA/sync/AlternatingSync.java
@@ -26,6 +26,7 @@ public class AlternatingSync extends SyncTestCaseCD2DB {
 		assertPostcondition("in/04_SubClassToTable_FWD", "expected/04_SubClassToTable_FWD");
 	}
 
+	@Ignore("TODO: Problems with deletion - non-deterministic!")
 	@Test
 	public void testCreateSubClassDeleteSubTable() {
 		createInheritance();
