19f885df5e6028385a19842a427ac7bdc156aaba
Anthony Anjorin <anthony.anjorin@upb.de>
2018-06-29 17:49:15 +0200

    Removed commented out code

--- tggs/AbcToNothing/src/org/emoflon/ibex/tgg/run/abctonothing/_RegistrationHelper.java
+++ tggs/AbcToNothing/src/org/emoflon/ibex/tgg/run/abctonothing/_RegistrationHelper.java
@@ -11,18 +11,6 @@ public class _RegistrationHelper {
 
 	/** Load and register source and target metamodels */
 	public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy)  throws IOException {
-		//throw new NotImplementedException("You need to register your source and target metamodels.");
-		
-		// For both source and target metamodels (and any other dependencies you might require)
-		
-		// Option 1 (recommended): If you have generated code for your metamodel <Foo> and use eMoflon projects and defaults,
-		//                         just add the project Foo as a plugin dependency and simply use:
-		//AbcPackageImpl.init();
-
-		// Option 2:  If you wish to use the .ecore file directly without generating code
-		// strategy.loadAndRegisterMetamodel("<pathToEcoreFile>");
-		
-		// Option 3 (advanced): If you have an .ecore file with an arbitrary URI "<URIOfPackage>"
 		String pathToEcoreFile = "platform:/resource/AbcToNothing/metamodels/Abc.ecore";
 		strategy.loadAndRegisterMetamodel(pathToEcoreFile);		
 	}
