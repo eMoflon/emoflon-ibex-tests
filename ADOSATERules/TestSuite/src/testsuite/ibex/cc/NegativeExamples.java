cba979ff1d98019bf4aaa68720cd1dc810d4d6f4
vipasyan <vipasyan56@gmail.com>
2019-02-20 01:15:54 +0100

    Adding TestSuite folder

---
+++ ADOSATERules/TestSuite/src/testsuite/ibex/cc/NegativeExamples.java
@@ -0,0 +1,14 @@
+package testsuite.ibex.cc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NegativeExamples {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ADOSATERules/TestSuite/src/testsuite/ibex/cc/PositiveExamples.java
@@ -0,0 +1,14 @@
+package testsuite.ibex.cc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PositiveExamples {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
