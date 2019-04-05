44e080bab07ed7b7f0a5548de0fa6eb21fbb3a0f
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-04-05 17:14:24 +0200

    Cleaned up the code (no semantic changes)

--- ADOSATERules/Adele2Aadl/src/org/emoflon/ibex/tgg/run/adele2aadl/_RegistrationHelper.java
+++ ADOSATERules/Adele2Aadl/src/org/emoflon/ibex/tgg/run/adele2aadl/_RegistrationHelper.java
@@ -2,17 +2,13 @@ package org.emoflon.ibex.tgg.run.adele2aadl;
 
 import java.io.IOException;
 
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.emoflon.ibex.tgg.operational.csp.constraints.factories.adele2aadl.UserDefinedRuntimeTGGAttrConstraintFactory;
 import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
 import org.emoflon.ibex.tgg.operational.strategies.OperationalStrategy;
 import org.osate.aadl2.impl.Aadl2PackageImpl;
-import org.osate.aadl2.impl.AadlPackageImpl;
 import org.osate.aadl2.instance.impl.InstancePackageImpl;
-// import org.osate.aadl2.impl.Aadl2PackageImpl;
-// import org.osate.aadl2.instance.impl.InstancePackageImpl;
 import org.topcased.adele.model.ADELE_Components.impl.ADELE_ComponentsPackageImpl;
 import org.topcased.adele.model.ADELE_Features.impl.ADELE_FeaturesPackageImpl;
 import org.topcased.adele.model.ADELE_Relations.impl.ADELE_RelationsPackageImpl;
@@ -26,46 +22,36 @@ public class _RegistrationHelper {
 
 	/** Load and register source and target metamodels */
 	public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy) throws IOException {
-		
-
 		EPackage adele_comp = ADELE_ComponentsPackageImpl.init();
-
 		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/ADELE_Components.ecore", adele_comp);
+		
 		EPackage adele_feat = ADELE_FeaturesPackageImpl.init();
 		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/ADELE_Features.ecore", adele_feat);
-		
-		
+
 		EPackage adele_rel = ADELE_RelationsPackageImpl.init();
 		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/ADELE_Relations.ecore", adele_rel);
-		
-		EPackage ba_comp = Ba_componentsPackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/BA_components.ecore",ba_comp);
 
-		
+		EPackage ba_comp = Ba_componentsPackageImpl.init();
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/BA_components.ecore", ba_comp);
+
 		EPackage ba_rel = Ba_relationsPackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/BA_relations.ecore",ba_rel);
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/BA_relations.ecore", ba_rel);
 
 		EPackage ba_feat = Ba_featuresPackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/BA_features.ecore",ba_feat );
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/BA_features.ecore", ba_feat);
 
-		
 		EPackage ker_sp = KernelSpicesPackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/KernelSpices.ecore",ker_sp);
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/KernelSpices.ecore", ker_sp);
 
-		
-		EPackage obj_des= ObjectDescriptionModelPackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/ObjectDescriptionModel.ecore",obj_des );
+		EPackage obj_des = ObjectDescriptionModelPackageImpl.init();
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/src/ObjectDescriptionModel.ecore",
+				obj_des);
 
-		EPackage aadl_trg= Aadl2PackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/trg/aadl2.ecore",aadl_trg);
+		EPackage aadl_trg = Aadl2PackageImpl.init();
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/trg/aadl2.ecore", aadl_trg);
 		
-		EPackage aadl_inst= InstancePackageImpl.init();
-		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/trg/instance.ecore",aadl_inst);
-		
-//		strategy.loadAndRegisterMetamodel("platform:/resource/Adele2Aadl/metamodels/trg/aadl2.ecore");
-//		strategy.loadAndRegisterMetamodel("platform:/resource/Adele2Aadl/metamodels/trg/instance.ecore");
-//		
-
+		EPackage aadl_inst = InstancePackageImpl.init();
+		rs.getPackageRegistry().put("platform:/resource/Adele2Aadl/metamodels/trg/instance.ecore", aadl_inst);
 	}
 
 	/** Create default options **/
