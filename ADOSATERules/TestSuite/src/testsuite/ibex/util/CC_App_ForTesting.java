de7dcaf6c2b3f235911126ae1705494ddae2297a
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-04-05 17:13:56 +0200

    Resource factory is not working

--- ADOSATERules/TestSuite/src/testsuite/ibex/util/CC_App_ForTesting.java
+++ ADOSATERules/TestSuite/src/testsuite/ibex/util/CC_App_ForTesting.java
@@ -7,10 +7,7 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.operational.strategies.opt.cc.CC;
 import org.emoflon.ibex.tgg.run.adele2aadl._RegistrationHelper;
 import org.emoflon.ibex.tgg.runtime.engine.DemoclesTGGEngine;
-import org.osate.aadl2.impl.Aadl2PackageImpl;
-import org.osate.aadl2.impl.AadlPackageImpl;
 import org.osate.aadl2.util.Aadl2ResourceFactoryImpl;
-import org.topcased.adele.model.ADELE_Components.impl.ADELE_ComponentsPackageImpl;
 import org.topcased.adele.model.ADELE_Components.util.ADELE_ComponentsResourceFactoryImpl;
 
 public class CC_App_ForTesting extends CC {
@@ -34,7 +31,7 @@ public class CC_App_ForTesting extends CC {
 		EcoreUtil.resolveAll(rs);
 	}
 
-	private Resource loadAdeleResource(String path) throws IOException {		
+	private Resource loadAdeleResource(String path) throws IOException {
 		getResourceSet().getResourceFactoryRegistry().getExtensionToFactoryMap().put("adele",
 				new ADELE_ComponentsResourceFactoryImpl());
 
@@ -42,10 +39,11 @@ public class CC_App_ForTesting extends CC {
 
 		return r;
 	}
-	private Resource loadAadlResource(String path) throws IOException {		
-		getResourceSet().getResourceFactoryRegistry().getExtensionToFactoryMap().put("aadl",
-				new Aadl2ResourceFactoryImpl());
 
+	private Resource loadAadlResource(String path) throws IOException {
+//		getResourceSet().getResourceFactoryRegistry().getExtensionToFactoryMap().put("aadl",
+//				new Aadl2ResourceFactoryImpl());
+//
 		Resource r = loadResource(path);
 
 		return r;
