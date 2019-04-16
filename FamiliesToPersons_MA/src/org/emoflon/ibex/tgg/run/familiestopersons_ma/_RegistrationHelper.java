a27e4b7d9c6ddb0e224bf369adb73e1f5c1e84a2
asher636 <asher@syqlo.com>
2019-04-16 20:10:58 +0200

    registering observers for monitors

--- FamiliesToPersons_MA/src/org/emoflon/ibex/tgg/run/familiestopersons_ma/_RegistrationHelper.java
+++ FamiliesToPersons_MA/src/org/emoflon/ibex/tgg/run/familiestopersons_ma/_RegistrationHelper.java
@@ -2,6 +2,8 @@ package org.emoflon.ibex.tgg.run.familiestopersons_ma;
 
 import java.io.IOException;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -16,42 +18,44 @@ import Families.impl.FamiliesPackageImpl;
 import Persons.impl.PersonsPackageImpl;
 
 public class _RegistrationHelper {
-
-	/** Load and register source and target metamodels */
-	public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy) throws IOException {
-		// Load and register source and target metamodels
-		EPackage familyPack = null;
-		EPackage personsPack = null;
-
-		if (strategy instanceof FWD_OPT) {
-			Resource res = strategy.loadResource(
-					"platform:/resource/../benchmarx/examples/familiestopersons/metamodels/Persons/model/Persons.ecore");
-			personsPack = (EPackage) res.getContents().get(0);
-			rs.getResources().remove(res);
-		} else if (strategy instanceof BWD_OPT) {
-			Resource res = strategy.loadResource(
-					"platform:/resource/../benchmarx/examples/familiestopersons/metamodels/Families/model/Families.ecore");
-			familyPack = (EPackage) res.getContents().get(0);
-			rs.getResources().remove(res);
-		}
-		
-		if(familyPack == null)
-			familyPack = FamiliesPackageImpl.init();
-		
-		if(personsPack == null)
-			personsPack = PersonsPackageImpl.init();
-
-		rs.getPackageRegistry().put("platform:/resource/Families/model/Families.ecore", familyPack);
-		rs.getPackageRegistry().put("platform:/resource/Persons/model/Persons.ecore", personsPack);
-	}
-
-	/** Create default options **/
-	public static IbexOptions createIbexOptions() {
-		IbexOptions options = new IbexOptions();
-		options.projectName("FamiliesToPersons_MA");
-		options.projectPath("FamiliesToPersons_MA");
-		options.debug(false);
-		options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
-		return options;
-	}
+	 
+    /** Load and register source and target metamodels */
+    public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy) throws IOException {
+        // Load and register source and target metamodels
+        EPackage familyPack = null;
+        EPackage personsPack = null;
+ 
+        if (strategy instanceof FWD_OPT) {
+            Resource res = strategy.loadResource(
+                    "platform:/resource/../../benchmarx/examples/familiestopersons/metamodels/Persons/model/Persons.ecore");
+            personsPack = (EPackage) res.getContents().get(0);
+            rs.getResources().remove(res);
+        } else if (strategy instanceof BWD_OPT) {
+            Resource res = strategy.loadResource(
+                    "platform:/resource/../../benchmarx/examples/familiestopersons/metamodels/Families/model/Families.ecore");
+            familyPack = (EPackage) res.getContents().get(0);
+            rs.getResources().remove(res);
+        }
+       
+        if(familyPack == null)
+            familyPack = FamiliesPackageImpl.init();
+       
+        if(personsPack == null)
+            personsPack = PersonsPackageImpl.init();
+ 
+        rs.getPackageRegistry().put("platform:/resource/Families/model/Families.ecore", familyPack);
+        rs.getPackageRegistry().put("platform:/resource/Persons/model/Persons.ecore", personsPack);
+    }
+ 
+    /** Create default options **/
+    public static IbexOptions createIbexOptions() {
+        IbexOptions options = new IbexOptions();
+        options.projectName("FamiliesToPersons_MA");
+        options.projectPath("FamiliesToPersons_MA");
+        options.debug(false);
+        options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+        Logger.getRootLogger().setLevel(Level.INFO);
+        options.debug(true);
+        return options;
+    }
 }
--- Testsuite/test/testsuite/ibex/FamiliesToPersons_MA/co/TestFamiliesToPersons_MA.java
+++ Testsuite/test/testsuite/ibex/FamiliesToPersons_MA/co/TestFamiliesToPersons_MA.java
@@ -2,6 +2,10 @@ package testsuite.ibex.FamiliesToPersons_MA.co;
 
 import java.io.IOException;
 
+import org.emoflon.ibex.tgg.operational.monitoring.GeneratedPatternsSizeObserver;
+import org.emoflon.ibex.tgg.operational.monitoring.MemoryConsumptionObserver;
+import org.emoflon.ibex.tgg.operational.monitoring.ModelSizeObserver;
+import org.emoflon.ibex.tgg.operational.monitoring.NumberOfMatchesObserver;
 import org.emoflon.ibex.tgg.run.familiestopersons_ma.BWD_OPT_App;
 import org.emoflon.ibex.tgg.run.familiestopersons_ma.CO_App;
 import org.emoflon.ibex.tgg.run.familiestopersons_ma.FWD_OPT_App;
@@ -13,17 +17,36 @@ import testsuite.ibex.testUtil.COTestCase;
 
 public class TestFamiliesToPersons_MA extends COTestCase {
 
-	public void createGenerator(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
-		checker = new CO_App("FamiliesToPersons_MA", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, this.ilpSolver);
-	}
-	
-	public void createTransformation(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
-		forward = new FWD_OPT_App("FamiliesToPersons_MA", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, this.ilpSolver);
-	}
-	
-	public void createBackward(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
-		backward = new BWD_OPT_App("FamiliesToPersons_MA", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, this.ilpSolver);
-	}
+	 public void createGenerator(String srcInstance, String trgInstance, String corrInstance, String protInstance)
+	            throws IOException {
+	        checker = new CO_App("FamiliesToPersons_MA", testsuite.ibex.performance.util.PerformanceConstants.workspacePath,
+	                false, srcInstance, trgInstance, corrInstance, protInstance, this.ilpSolver);
+	        checker.registerObserver(new GeneratedPatternsSizeObserver (checker));
+	        checker.registerObserver(new ModelSizeObserver (checker));
+	        checker.registerObserver(new NumberOfMatchesObserver (checker));
+	        checker.registerObserver(new MemoryConsumptionObserver (checker));
+	    }
+	 
+	    public void createTransformation(String srcInstance, String trgInstance, String corrInstance, String protInstance)
+	            throws IOException {
+	        forward = new FWD_OPT_App("FamiliesToPersons_MA",
+	                testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance,
+	                corrInstance, protInstance, this.ilpSolver);
+	        forward.registerObserver(new GeneratedPatternsSizeObserver (forward));
+	        forward.registerObserver(new ModelSizeObserver (forward));
+	        forward.registerObserver(new NumberOfMatchesObserver (forward));
+	        forward.registerObserver(new MemoryConsumptionObserver (forward));
+	    }
+	 
+	    public void createBackward(String srcInstance, String trgInstance, String corrInstance, String protInstance)
+	            throws IOException {
+	        backward = new BWD_OPT_App("FamiliesToPersons_MA",
+	                testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance,
+	                corrInstance, protInstance, this.ilpSolver);
+	        backward.registerObserver(new ModelSizeObserver (backward));
+	        backward.registerObserver(new NumberOfMatchesObserver (backward));
+	        backward.registerObserver(new MemoryConsumptionObserver (backward));
+	    }
 	
 	@Test
 	public void testFWD_OPT() throws IOException {
