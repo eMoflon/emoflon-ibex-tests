09e6d97bdde86a9f3ae59bf9b7257522089e5faf
arg0n1s <basti_seppl@yahoo.com>
2019-07-11 16:11:01 +0200

    fixed old _RegistrationHelper in BlockDiagramAdapter_EdgeRules

--- BlockDiagramCodeAdapter_EdgeRules/src/org/emoflon/ibex/tgg/run/blockdiagramcodeadapter_edgerules/_RegistrationHelper.java
+++ BlockDiagramCodeAdapter_EdgeRules/src/org/emoflon/ibex/tgg/run/blockdiagramcodeadapter_edgerules/_RegistrationHelper.java
@@ -19,28 +19,11 @@ public class _RegistrationHelper {
 	/** Load and register source and target metamodels */
 	public static void registerMetamodels(ResourceSet rs, OperationalStrategy strategy)  throws IOException {
 		// Load and register source and target metamodels
-		BlockDiagramPackageImpl.init();
-		MocaTreePackageImpl.init();
-		
-		BlockDiagramCodeAdapter_EdgeRulesPackageImpl.init();
-		
-		rs.getPackageRegistry().put("platform:/resource/BlockDiagramCodeAdapter_EdgeRules/model/BlockDiagramCodeAdapter_EdgeRules.ecore", BlockDiagramCodeAdapter_EdgeRulesPackage.eINSTANCE);
-		rs.getPackageRegistry().put("platform:/plugin/BlockDiagramCodeAdapter_EdgeRules/model/BlockDiagramCodeAdapter_EdgeRules.ecore", BlockDiagramCodeAdapter_EdgeRulesPackage.eINSTANCE);
-		
-		if(strategy instanceof FWD_OPT) {
-		}
-		
-		if(strategy instanceof BWD_OPT) {
-		}
+		DefaultRegistrationHelper.registerMetamodels(rs, strategy);
 	}
 	
 	/** Create default options **/
 	public static IbexOptions createIbexOptions() {
-		IbexOptions options = new IbexOptions();
-		options.projectName("BlockDiagramCodeAdapter_EdgeRules");
-		options.projectPath("BlockDiagramCodeAdapter_EdgeRules");
-		options.debug(false);
-		options.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
-		return options;
+		return DefaultRegistrationHelper.createIbexOptions();
 	}
 }
