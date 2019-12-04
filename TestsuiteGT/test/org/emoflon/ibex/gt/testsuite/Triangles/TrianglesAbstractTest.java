327831542a4e0154d741457632683e4d28b00d8b
arg0n1s <basti_seppl@yahoo.com>
2019-12-04 18:34:18 +0100

    added triangle pattern tests

---
+++ TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/Triangles/TrianglesAbstractTest.java
@@ -0,0 +1,24 @@
+package org.emoflon.ibex.gt.testsuite.Triangles;
+
+import org.emoflon.ibex.gt.testsuite.GTAppTestCase;
+
+import TrianglesGraphTransformation.api.TrianglesGraphTransformationAPI;
+import TrianglesGraphTransformation.api.TrianglesGraphTransformationApp;
+
+/**
+ * Abstract test class for the ClassMultipleInheritanceHierarchy Graph
+ * Transformation API. All tests for this API should inherit from this class.
+ */
+public class TrianglesAbstractTest extends
+		GTAppTestCase<TrianglesGraphTransformationApp, TrianglesGraphTransformationAPI> {
+
+	@Override
+	protected String getTestName() {
+		return "Triangles";
+	}
+
+	@Override
+	protected TrianglesGraphTransformationApp getApp() {
+		return new TrianglesGraphTransformationApp(initEngine(), workspacePath);
+	}
+}
