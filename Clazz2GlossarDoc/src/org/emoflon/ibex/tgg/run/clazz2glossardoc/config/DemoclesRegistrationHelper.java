440eb3d3e119377a4d8d7f626b737c0fe110a1cc
Lars <lars.fritsche@es.tu-darmstadt.de>
2020-03-16 11:27:54 +0100

    new example for model integration

---
+++ Clazz2GlossarDoc/src/org/emoflon/ibex/tgg/run/clazz2glossardoc/config/DemoclesRegistrationHelper.java
@@ -0,0 +1,31 @@
+package org.emoflon.ibex.tgg.run.clazz2glossardoc.config;
+
+import java.io.IOException;
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.emoflon.ibex.tgg.operational.csp.constraints.factories.clazz2glossardoc.UserDefinedRuntimeTGGAttrConstraintFactory;
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
+import org.emoflon.ibex.tgg.operational.strategies.modules.IbexExecutable;
+import org.emoflon.ibex.tgg.runtime.democles.DemoclesTGGEngine;
+
+public class DemoclesRegistrationHelper implements IRegistrationHelper {
+
+	/** Load and register source and target metamodels */
+	public void registerMetamodels(ResourceSet rs, IbexExecutable executable) throws IOException {
+		// Replace to register generated code or handle other URI-related requirements
+		executable.getResourceHandler().loadAndRegisterMetamodel("platform:/resource/SimpleClassInheritance/model/simpleClassInheritance.ecore");
+	}
+
+	/** Create default options **/
+	public IbexOptions createIbexOptions() {
+		IbexOptions options = new IbexOptions();
+		options.blackInterpreter(new DemoclesTGGEngine());
+		options.project.name("Clazz2GlossarDoc");
+		options.project.path("Clazz2GlossarDoc");
+		options.debug.ibexDebug(false);
+		options.csp.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+		options.registrationHelper(this);
+		return options;
+	}
+}
