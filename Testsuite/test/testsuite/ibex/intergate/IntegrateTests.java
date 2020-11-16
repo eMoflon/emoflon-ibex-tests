4f5d4c3f0d5a8a87be68b9e1e68c8455f7251640
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2020-11-16 15:18:18 +0100

    Add TestSuite class for running all integrate tests together

---
+++ Testsuite/test/testsuite/ibex/intergate/IntegrateTests.java
@@ -0,0 +1,19 @@
+package testsuite.ibex.intergate;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ //
+		testsuite.ibex.Clazz2GlossarDoc.integrate.Basic.class, //
+		testsuite.ibex.Clazz2GlossarDoc.integrate.Combined.class, //
+		testsuite.ibex.Clazz2GlossarDoc.integrate.Move.class, //
+		testsuite.ibex.CompanyToIT.integrate.Basic.class, //
+		testsuite.ibex.Java2Doc.integrate.Del.class, //
+		testsuite.ibex.Java2Doc.integrate.DeletePreserve.class, //
+		testsuite.ibex.TerraceHouses2BlockSet.integrate.Multiplicity.class //
+})
+public class IntegrateTests {
+
+}
