e722a1f443fd74c13b3187c5764086bab37c42af
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2021-04-26 19:19:27 +0200

    Cross-over LocalCC test

--- Testsuite/test/testsuite/ibex/Java2Doc/integrate/LocalCCTest.java
+++ Testsuite/test/testsuite/ibex/Java2Doc/integrate/LocalCCTest.java
@@ -3,7 +3,6 @@ package testsuite.ibex.Java2Doc.integrate;
 import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.APPLY_USER_DELTA;
 import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.CHECK_LOCAL_CONSISTENCY;
 import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.CLEAN_UP;
-import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.REPAIR;
 import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.RESOLVE_BROKEN_MATCHES;
 import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.RESOLVE_CONFLICTS;
 import static org.emoflon.ibex.tgg.operational.strategies.integrate.FragmentProvider.TRANSLATE;
@@ -12,23 +11,23 @@ import java.util.Arrays;
 
 import org.benchmarx.simpledoc.core.SimpleDocHelper;
 import org.benchmarx.simpledoc.core.SimpleJavaHelper;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.operational.strategies.integrate.pattern.IntegrationPattern;
 import org.junit.Test;
 
 import simpleDoc.Folder;
+import simpleJava.Clazz;
 import simpleJava.Package;
 import testsuite.ibex.Java2Doc.integrate.util.IntegIbexJava2Doc;
 import testsuite.ibex.testUtil.IntegrateTestCase;
 
-public class LocalCC extends IntegrateTestCase<Package, Folder> {
+public class LocalCCTest extends IntegrateTestCase<Package, Folder> {
 
 	public final static String projectName = "Java2Doc";
 
 	SimpleJavaHelper helperJava;
 	SimpleDocHelper helperDoc;
 
-	public LocalCC() {
+	public LocalCCTest() {
 		super(new IntegIbexJava2Doc(projectName, "moflon"));
 	}
 
@@ -45,7 +44,6 @@ public class LocalCC extends IntegrateTestCase<Package, Folder> {
 
 	private final IntegrationPattern pattern = new IntegrationPattern(Arrays.asList( //
 			APPLY_USER_DELTA //
-//			, REPAIR //
 			, CHECK_LOCAL_CONSISTENCY //
 			, RESOLVE_CONFLICTS //
 			, RESOLVE_BROKEN_MATCHES //
@@ -64,6 +62,31 @@ public class LocalCC extends IntegrateTestCase<Package, Folder> {
 			helperJava.createClass(helperJava.getPackage(p, "cmoflon"), "ccClazz", "ccContent");
 			// trg:
 			helperDoc.createDoc(helperDoc.getFolder(f, "cmoflon"), "ccClazz_doc", "ccBody");
+		});
+
+		assertCondition(fullPath + "src", fullPath + "trg", fullPath + "corr");
+	}
+
+	@Test
+	public void lcc_crossover() {
+		final String fullPathPre = testpath + "lcc_crossover_pre/";
+		final String fullPath = testpath + "lcc_crossover/";
+		tool.getOptions().integration.pattern(pattern);
+		tool.applyAndIntegrateDelta((p, f) -> {
+			// src:
+			helperJava.createClass(helperJava.getPackage(p, "es"), "EMFUtil", "EMFUtilImpl");
+			// trg:
+			helperDoc.createDoc(helperDoc.getFolder(f, "es"), "EMFUtil_doc", "EMFUtilBody");
+		});
+
+		assertCondition(fullPathPre + "src", fullPathPre + "trg", fullPathPre + "corr");
+
+		tool.applyAndIntegrateDelta((p, f) -> {
+			// src:
+			Clazz ecoreUtil = helperJava.getClazz(p, "EcoreUtil");
+			Clazz emfUtil = helperJava.getClazz(p, "EMFUtil");
+			ecoreUtil.setName("EMFUtil");
+			emfUtil.setName("CommonUtil");
 		});
 
 		assertCondition(fullPath + "src", fullPath + "trg", fullPath + "corr");
