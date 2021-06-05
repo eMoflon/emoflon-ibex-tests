46f23c0a37880399d6fa2ecd3a49d6ec371a23e1
isabella-grieser <isabella.nunesgrieser@stud.tu-darmstadt.de>
2021-06-05 15:16:32 +0200

    everythin else

---
+++ AbcToNothing/src/org/emoflon/ibex/tgg/run/abctonothing/config/DemoclesRegistrationHelper.java
@@ -0,0 +1,31 @@
+package org.emoflon.ibex.tgg.run.abctonothing.config;
+
+import java.io.IOException;
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.emoflon.ibex.tgg.operational.csp.constraints.factories.abctonothing.UserDefinedRuntimeTGGAttrConstraintFactory;
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
+import org.emoflon.ibex.tgg.operational.strategies.modules.IbexExecutable;
+import org.emoflon.ibex.tgg.runtime.democles.DemoclesTGGEngine;
+
+public class DemoclesRegistrationHelper implements IRegistrationHelper {
+
+	/** Load and register source and target metamodels */
+	public void registerMetamodels(ResourceSet rs, IbexExecutable executable) throws IOException {
+		// Replace to register generated code or handle other URI-related requirements
+		executable.getResourceHandler().loadAndRegisterMetamodel("platform:/resource/AbcToNothing/metamodels/Abc.ecore");
+	}
+
+	/** Create default options **/
+	public IbexOptions createIbexOptions() {
+		IbexOptions options = new IbexOptions();
+		options.blackInterpreter(new DemoclesTGGEngine());
+		options.project.name("AbcToNothing");
+		options.project.path("AbcToNothing");
+		options.debug.ibexDebug(false);
+		options.csp.userDefinedConstraints(new UserDefinedRuntimeTGGAttrConstraintFactory());
+		options.registrationHelper(this);
+		return options;
+	}
+}
