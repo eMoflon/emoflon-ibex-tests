2b2e14a3ca076db3e4aa5c877fd6eca4fb76298b
Lars Fritsche <lars.fritsche@es.tu-darmstadt.de>
2022-01-18 14:59:31 +0100

    removed tgg natures

--- Testsuite/test/testsuite/ibex/MoDiscoIbexTGG/co/TestMoDiscoIbexTGG.java
+++ Testsuite/test/testsuite/ibex/MoDiscoIbexTGG/co/TestMoDiscoIbexTGG.java
@@ -3,11 +3,11 @@ package testsuite.ibex.MoDiscoIbexTGG.co;
 import java.io.IOException;
 
 import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
-import org.emoflon.ibex.tgg.run.modiscoibextgg.CO_App;
-import org.emoflon.ibex.tgg.run.modiscoibextgg.FWD_OPT_App;
-import org.emoflon.ibex.tgg.run.modiscoibextgg.BWD_OPT_App;
-import org.emoflon.ibex.tgg.run.modiscoibextgg.config.DemoclesRegistrationHelper;
-import org.emoflon.ibex.tgg.run.modiscoibextgg.config.ViatraRegistrationHelper;
+//import org.emoflon.ibex.tgg.run.modiscoibextgg.CO_App;
+//import org.emoflon.ibex.tgg.run.modiscoibextgg.FWD_OPT_App;
+//import org.emoflon.ibex.tgg.run.modiscoibextgg.BWD_OPT_App;
+//import org.emoflon.ibex.tgg.run.modiscoibextgg.config.DemoclesRegistrationHelper;
+//import org.emoflon.ibex.tgg.run.modiscoibextgg.config.ViatraRegistrationHelper;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,52 +16,52 @@ import testsuite.ibex.testUtil.UsedPatternMatcher;
 
 public class TestMoDiscoIbexTGG extends COTestCase {
 
-	public void createGenerator(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
-		CO_App.registrationHelper = UsedPatternMatcher.choose(new IRegistrationHelper[]{new DemoclesRegistrationHelper() /*new HiPERegistrationHelper()*/, new ViatraRegistrationHelper()});
-		checker = new CO_App("MoDiscoIbexTGG", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, ilpSolver);
-	}
+//	public void createGenerator(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
+//		CO_App.registrationHelper = UsedPatternMatcher.choose(new IRegistrationHelper[]{new DemoclesRegistrationHelper() /*new HiPERegistrationHelper()*/, new ViatraRegistrationHelper()});
+//		checker = new CO_App("MoDiscoIbexTGG", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, ilpSolver);
+//	}
+//	
+//	public void createTransformation(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
+//		FWD_OPT_App.registrationHelper = UsedPatternMatcher.choose(new IRegistrationHelper[]{new DemoclesRegistrationHelper() /*new HiPERegistrationHelper()*/, new ViatraRegistrationHelper()});
+//		forward = new FWD_OPT_App("MoDiscoIbexTGG", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, ilpSolver);
+//	}
+//	
+//	public void createBackward(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
+//		BWD_OPT_App.registrationHelper = UsedPatternMatcher.choose(new IRegistrationHelper[]{new DemoclesRegistrationHelper() /*new HiPERegistrationHelper()*/, new ViatraRegistrationHelper()});
+//		backward = new BWD_OPT_App("MoDiscoIbexTGG", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, ilpSolver);
+//	}
 	
-	public void createTransformation(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
-		FWD_OPT_App.registrationHelper = UsedPatternMatcher.choose(new IRegistrationHelper[]{new DemoclesRegistrationHelper() /*new HiPERegistrationHelper()*/, new ViatraRegistrationHelper()});
-		forward = new FWD_OPT_App("MoDiscoIbexTGG", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, ilpSolver);
-	}
-	
-	public void createBackward(String srcInstance, String trgInstance, String corrInstance, String protInstance) throws IOException {
-		BWD_OPT_App.registrationHelper = UsedPatternMatcher.choose(new IRegistrationHelper[]{new DemoclesRegistrationHelper() /*new HiPERegistrationHelper()*/, new ViatraRegistrationHelper()});
-		backward = new BWD_OPT_App("MoDiscoIbexTGG", testsuite.ibex.performance.util.PerformanceConstants.workspacePath, false, srcInstance, trgInstance, corrInstance, protInstance, ilpSolver);
-	}
-	
-	@Test
-	public void testFWD_OPT() throws IOException {
-		createTransformation("/resources/co/src", "/resources/co/trg-tmp", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
-		runFWD_OPT();
-		createGenerator("/resources/co/src", "/resources/co/trg-tmp", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
-		runCO();
-		Assert.assertTrue(checker.modelsAreConsistent());
-	}
-	
-
-	@Test
-	public void testBWD_OPT() throws IOException {
-		createBackward("/resources/co/src-tmp", "/resources/co/trg", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
-		runBWD_OPT();
-		createGenerator("/resources/co/src-tmp", "/resources/co/trg", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
-		runCO();
-		Assert.assertTrue(checker.modelsAreConsistent());
-	}
-	
-	@Test
-	public void testSimplePositive() throws IOException {
-		createGenerator("/resources/co/src", "/resources/co/trg", "/resources/co/corr", "/resources/co/protocol");
-		runCO();
-		Assert.assertTrue(checker.modelsAreConsistent());
-	}
-	
-	@Test
-	public void testMissingLink() throws IOException {
-		createGenerator("/resources/co/src", "/resources/co/trg", "/resources/co/corr_inc", "/resources/co/protocol");
-		runCO();
-		Assert.assertFalse(checker.modelsAreConsistent());
-	}
+//	@Test
+//	public void testFWD_OPT() throws IOException {
+//		createTransformation("/resources/co/src", "/resources/co/trg-tmp", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
+//		runFWD_OPT();
+//		createGenerator("/resources/co/src", "/resources/co/trg-tmp", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
+//		runCO();
+//		Assert.assertTrue(checker.modelsAreConsistent());
+//	}
+//	
+//
+//	@Test
+//	public void testBWD_OPT() throws IOException {
+//		createBackward("/resources/co/src-tmp", "/resources/co/trg", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
+//		runBWD_OPT();
+//		createGenerator("/resources/co/src-tmp", "/resources/co/trg", "/resources/co/corr-tmp", "/resources/co/prot-tmp");
+//		runCO();
+//		Assert.assertTrue(checker.modelsAreConsistent());
+//	}
+//	
+//	@Test
+//	public void testSimplePositive() throws IOException {
+//		createGenerator("/resources/co/src", "/resources/co/trg", "/resources/co/corr", "/resources/co/protocol");
+//		runCO();
+//		Assert.assertTrue(checker.modelsAreConsistent());
+//	}
+//	
+//	@Test
+//	public void testMissingLink() throws IOException {
+//		createGenerator("/resources/co/src", "/resources/co/trg", "/resources/co/corr_inc", "/resources/co/protocol");
+//		runCO();
+//		Assert.assertFalse(checker.modelsAreConsistent());
+//	}
 }
 	
