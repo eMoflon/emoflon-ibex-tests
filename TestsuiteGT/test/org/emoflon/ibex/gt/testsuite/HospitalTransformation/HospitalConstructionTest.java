3ed73a956b0e3948123351ceb345e680ef6af7c8
SebastianE <sebastian.ehmes@gmail.com>
2022-11-01 20:27:12 +0100

    + added Hospital-GT as next GTL project

--- TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/HospitalTransformation/HospitalConstructionTest.java
+++ TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/HospitalTransformation/HospitalConstructionTest.java
@@ -5,64 +5,67 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import HospitalExample.Carelevel;
-import hospital.HospitalTransformation.api.HospitalTransformationAPI;
-import hospital.example.HospitalRules;
-
+import hospital.gt.api.GtHiPEGtAPI;
+import hospital.util.HospitalRules;
 public class HospitalConstructionTest extends HospitalTransformationAbstractTest {
 
 	@Test
 	public void constructHospital1() {
-		HospitalTransformationAPI api = this.init("HospitalConstruction.xmi");
+		GtHiPEGtAPI api = this.initEmpty("HospitalConstruction.xmi");
 		
 		// Check axiomatic rule (rules with green(++) elements only) with NACs
 		assertMatchCount(1, api.hospital());
-		assertApplicable(api.hospital());
+		assertApplicableAndApply(api.hospital());
 		assertNotApplicable(api.hospital());
+		
+		saveAndTerminate(api);
 	}
 	
 	@Test
 	public void constructHospital2() {
-		HospitalTransformationAPI api = this.init("HospitalConstruction.xmi");
+		GtHiPEGtAPI api = this.initEmpty("HospitalConstruction.xmi");
 		
-		assertApplicable(api.hospital());
-		assertApplicable(api.reception());
+		assertApplicableAndApply(api.hospital());
+		assertApplicableAndApply(api.reception());
 		
 		for(int i=0; i<4; i++) {
-			assertApplicable(api.department(i+2, 4));
+			assertApplicableAndApply(api.department(i+2, 4));
 		}
 		for(int i=0; i<16; i++) {
-			assertApplicable(api.room(4, Carelevel.get(1)));
+			assertApplicableAndApply(api.room(4, Carelevel.get(1)));
 		}
 		
 		int staffID = 2;
-		while(api.findDepartmentWithoutDoctor().hasMatches()) {
-			assertApplicable(api.doctor(16, HospitalRules.firstNames[HospitalRules.firstNames.length%staffID]+" "+HospitalRules.lastNames[HospitalRules.lastNames.length%staffID], staffID++));
+		while(api.findDepartmentWithoutDoctor().hasMatches(true)) {
+			assertApplicableAndApply(api.doctor(16, HospitalRules.firstNames[HospitalRules.firstNames.length%staffID]+" "+HospitalRules.lastNames[HospitalRules.lastNames.length%staffID], staffID++));
 		}
 		
-		while(api.findRoomWithoutNurse().hasMatches()) {
-			assertApplicable(api.assignNurseToRoom(HospitalRules.firstNames[HospitalRules.firstNames.length%staffID]+" "+HospitalRules.lastNames[HospitalRules.lastNames.length%staffID], staffID++));
+		while(api.findRoomWithoutNurse().hasMatches(true)) {
+			assertApplicableAndApply(api.assignNurseToRoom(HospitalRules.firstNames[HospitalRules.firstNames.length%staffID]+" "+HospitalRules.lastNames[HospitalRules.lastNames.length%staffID], staffID++));
 		}
 		
 		int patientID = 2;
 		for(int i=16; i>0; i--) {
-			assertApplicable(api.patient(HospitalRules.firstNames[HospitalRules.firstNames.length%patientID]+" "+HospitalRules.lastNames[HospitalRules.lastNames.length%patientID], patientID++, Carelevel.PENDING));
+			assertApplicableAndApply(api.patient(HospitalRules.firstNames[HospitalRules.firstNames.length%patientID]+" "+HospitalRules.lastNames[HospitalRules.lastNames.length%patientID], patientID++, Carelevel.PENDING));
 		}
 	
-		while(api.findPatientInReception().hasMatches()) {
-			assertApplicable(api.assignPatientToRoom());
+		while(api.findPatientInReception().hasMatches(true)) {
+			assertApplicableAndApply(api.assignPatientToRoom());
 		}
 		
 		// Check result:
-		assertEquals(1, api.findHospital().countMatches());
-		assertEquals(1, api.findReception().countMatches());
-		assertEquals(4, api.findDepartment().countMatches());
-		assertEquals(16, api.findRoom().countMatches());
-		assertEquals(4, api.findDoctor().countMatches());
-		assertEquals(4, api.findDocWithPatient().countMatches());
-		assertEquals(16, api.findNurse().countMatches());
-		assertEquals(16, api.findNurseInRoom().countMatches());
-		assertEquals(16, api.findPatient().countMatches());
-		assertEquals(0, api.findPatientInReception().countMatches());
-		assertEquals(16, api.findPatientInRoom().countMatches());
+		assertEquals(1, api.findHospital().countMatches(true));
+		assertEquals(1, api.findReception().countMatches(false));
+		assertEquals(4, api.findDepartment().countMatches(false));
+		assertEquals(16, api.findRoom().countMatches(false));
+		assertEquals(4, api.findDoctor().countMatches(false));
+		assertEquals(4, api.findDocWithPatient().countMatches(false));
+		assertEquals(16, api.findNurse().countMatches(false));
+		assertEquals(16, api.findNurseInRoom().countMatches(false));
+		assertEquals(16, api.findPatient().countMatches(false));
+		assertEquals(0, api.findPatientInReception().countMatches(false));
+		assertEquals(16, api.findPatientInRoom().countMatches(false));
+		
+		saveAndTerminate(api);
 	}
 }
--- TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/HospitalTransformation/HospitalTransformationAbstractTest.java
+++ TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/HospitalTransformation/HospitalTransformationAbstractTest.java
@@ -2,10 +2,10 @@ package org.emoflon.ibex.gt.testsuite.HospitalTransformation;
 
 import org.emoflon.ibex.gt.testsuite.GTAppTestCase;
 
-import hospital.HospitalTransformation.api.HospitalTransformationAPI;
-import hospital.HospitalTransformation.api.HospitalTransformationApp;
+import hospital.gt.api.GtHiPEGtAPI;
+
 	
-public class HospitalTransformationAbstractTest extends GTAppTestCase<HospitalTransformationApp, HospitalTransformationAPI>{
+public class HospitalTransformationAbstractTest extends GTAppTestCase<GtHiPEGtAPI>{
 
 		@Override
 		protected String getTestName() {
@@ -13,8 +13,8 @@ public class HospitalTransformationAbstractTest extends GTAppTestCase<HospitalTr
 		}
 
 		@Override
-		protected HospitalTransformationApp getApp() {
-			return new HospitalTransformationApp(initEngine(), workspacePath);
+		protected GtHiPEGtAPI getApi() {
+			return new GtHiPEGtAPI();
 		}
 }
 
