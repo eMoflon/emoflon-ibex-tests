f3522578c1ac9d0478b9e5f3739ff3992dd72da5
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2023-03-24 12:28:48 +0100

    Transfer Java2Doc

--- Java2Doc/src/org/emoflon/ibex/tgg/run/java2doc/LocalCC_App.java
+++ Java2Doc/src/org/emoflon/ibex/tgg/run/java2doc/LocalCC_App.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
-import org.emoflon.ibex.tgg.operational.strategies.opt.LocalCC;
 import org.emoflon.ibex.tgg.run.java2doc.config._DefaultRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.config.IRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.strategies.opt.LocalCC;
 
 
 public class LocalCC_App extends LocalCC {
