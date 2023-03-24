c1f070b3cd4a553c700d00f97187f267145b8161
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2023-03-24 15:31:42 +0100

    Transfer TerraceHouses2BlockSet

--- TerraceHouses2BlockSet/src/org/emoflon/ibex/tgg/run/terracehouses2blockset/SYNC_App.java
+++ TerraceHouses2BlockSet/src/org/emoflon/ibex/tgg/run/terracehouses2blockset/SYNC_App.java
@@ -2,10 +2,10 @@ package org.emoflon.ibex.tgg.run.terracehouses2blockset;
 
 import java.io.IOException;
 
-import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
-import org.emoflon.ibex.tgg.operational.strategies.modules.TGGResourceHandler;
-import org.emoflon.ibex.tgg.operational.strategies.sync.SYNC;
 import org.emoflon.ibex.tgg.run.terracehouses2blockset.config._DefaultRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.config.IRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.strategies.modules.TGGResourceHandler;
+import org.emoflon.ibex.tgg.runtime.strategies.sync.SYNC;
 import org.emoflon.ibex.tgg.util.ilp.ILPFactory.SupportedILPSolver;
 
 public class SYNC_App extends SYNC {
