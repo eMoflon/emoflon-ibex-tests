b5173e628a2858b02bf0080153b0581aee8acb58
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2023-04-11 21:10:42 +0200

    Transfer BenchmarxFamiliesToPersons

--- BenchmarxFamiliesToPersons/src/org/emoflon/ibex/tgg/run/benchmarxfamiliestopersons/INITIAL_BWD_App.java
+++ BenchmarxFamiliesToPersons/src/org/emoflon/ibex/tgg/run/benchmarxfamiliestopersons/INITIAL_BWD_App.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
-import org.emoflon.ibex.tgg.operational.strategies.sync.INITIAL_BWD;
 import org.emoflon.ibex.tgg.run.benchmarxfamiliestopersons.config._DefaultRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.config.IRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.strategies.sync.INITIAL_BWD;
 
 public class INITIAL_BWD_App extends INITIAL_BWD {
 
--- BenchmarxFamiliesToPersons/src/org/emoflon/ibex/tgg/run/benchmarxfamiliestopersons/INITIAL_FWD_App.java
+++ BenchmarxFamiliesToPersons/src/org/emoflon/ibex/tgg/run/benchmarxfamiliestopersons/INITIAL_FWD_App.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
-import org.emoflon.ibex.tgg.operational.strategies.sync.INITIAL_FWD;
 import org.emoflon.ibex.tgg.run.benchmarxfamiliestopersons.config._DefaultRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.config.IRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.strategies.sync.INITIAL_FWD;
 
 public class INITIAL_FWD_App extends INITIAL_FWD {
 
