fd5c7eedca429cbf36d0b415285eefdb0c423047
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2023-04-12 17:39:23 +0200

    Transfer ExtType2Doc_LookAhead

--- ExtType2Doc_LookAhead/src/org/emoflon/ibex/tgg/run/exttype2doc_lookahead/CC_App.java
+++ ExtType2Doc_LookAhead/src/org/emoflon/ibex/tgg/run/exttype2doc_lookahead/CC_App.java
@@ -3,10 +3,10 @@ package org.emoflon.ibex.tgg.run.exttype2doc_lookahead;
 import java.io.IOException;
 import java.util.function.Function;
 
-import org.emoflon.ibex.tgg.compiler.defaults.IRegistrationHelper;
-import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
-import org.emoflon.ibex.tgg.operational.strategies.opt.CC;
 import org.emoflon.ibex.tgg.run.exttype2doc_lookahead.config.HiPERegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.config.IRegistrationHelper;
+import org.emoflon.ibex.tgg.runtime.config.options.IbexOptions;
+import org.emoflon.ibex.tgg.runtime.strategies.opt.CC;
 
 public class CC_App extends CC {
 
