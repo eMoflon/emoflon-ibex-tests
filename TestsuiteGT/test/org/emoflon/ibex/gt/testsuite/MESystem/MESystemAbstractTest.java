87cecc8b64bf4f61faab4f87162eff0b1ee85015
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2024-10-17 15:20:58 +0200

    Applied auto formatter + input organizer

--- TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/MESystem/MESystemAbstractTest.java
+++ TestsuiteGT/test/org/emoflon/ibex/gt/testsuite/MESystem/MESystemAbstractTest.java
@@ -5,10 +5,9 @@ import org.emoflon.ibex.gt.testsuite.GTAppTestCase;
 import mesystem.gt.api.GtGtApi;
 import mesystem.gt.api.GtHiPEGtApi;
 
-
 /**
- * Abstract test class for the MESystem. All tests for this
- * Api should inherit from this class.
+ * Abstract test class for the MESystem. All tests for this Api should inherit
+ * from this class.
  */
 public class MESystemAbstractTest extends GTAppTestCase<GtGtApi<?>> {
 
@@ -21,12 +20,16 @@ public class MESystemAbstractTest extends GTAppTestCase<GtGtApi<?>> {
 	protected GtGtApi<?> getApi() {
 		return getApi(patternMatcher);
 	}
-	
+
 	@Override
 	protected GtGtApi<?> getApi(String patternMatcher) {
-		return switch(patternMatcher) {
-			case PM_HIPE -> {yield new GtHiPEGtApi();}
-			default -> {yield new GtHiPEGtApi();}
+		return switch (patternMatcher) {
+		case PM_HIPE -> {
+			yield new GtHiPEGtApi();
+		}
+		default -> {
+			yield new GtHiPEGtApi();
+		}
 		};
 	}
 }
